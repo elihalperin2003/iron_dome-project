ff74382b859e22e118ccf081b9ddf421d634cab4
eli <eelihalperin@gmail.com>
2026-07-15 12:45:15 +0300

    creating in baseRepository function - update

--- repository/baseRepository.js
+++ repository/baseRepository.js
@@ -1,13 +1,13 @@
 import pool from "./db/database.js";
 
 function baseRepository(tableName) {
-  async function select(filter) {
-    const values = filter ? Object.values(filter) : "";
-    const conditions = filter
+  async function select(filters) {
+    const values = filters ? Object.values(filters) : "";
+    const conditions = filters
       ? " where " +
-        Object.keys(filter)
+        Object.keys(filters)
           .map((key) => `${key} = ?`)
-          .join(",")
+          .join(" and ")
       : "";
     const [result] = await pool.execute(
       `SELECT * FROM ${tableName} ${conditions}`,
@@ -16,14 +16,32 @@ function baseRepository(tableName) {
     return result;
   }
   async function insert(newLine) {
-    const values = Object.values(filter);
-    const keys = Object.keys(filter).join(", ");
+    const values = Object.values(newLine);
+    const keys = Object.keys(newLine).join(", ");
     const [result] = await pool.execute(
       `INSERT INTO ${tableName} (${keys}) VALUES (${values.map((_) => "?").join(",")})`,
       values,
     );
     return;
   }
+  async function update(newData, filters) {
+    const valuesFilters = filters ? Object.values(filters) : "";
+    const conditions = filters
+      ? " where " +
+        Object.keys(filters)
+          .map((key) => `${key} = ?`)
+          .join(" and ")
+      : "";
+    const valuesData = Object.values(newData);
+    const keysData = Object.keys(newData)
+      .map((key) => `${key} = ?`)
+      .join(", ");
+    const [result] = await pool.execute(
+      `UPDATE ${tableName} SET ${keysData} ${conditions}`,
+      [...valuesData, ...valuesFilters],
+    );
+    return;
+  }
 
-  return { select };
+  return { select, insert, update };
 }
